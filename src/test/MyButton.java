ce3c20d7bbad175a7e9fa6dacead2ee1c8305115
benjamin <benjamin.leplenier@gmail.com>
2022-09-16 22:58:09 +0200

    Modif via eclipse

--- src/test/MyButton.java
+++ src/test/MyButton.java
@@ -5,7 +5,7 @@ import javafx.scene.control.Button;
 public class MyButton extends Button {
   
   public String toString(){
-      return "A Simple Button";
+      return "A Simple Button modified on eclipse";
   }
 
 }
