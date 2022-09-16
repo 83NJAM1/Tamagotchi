a0b7d8f0fd149a5b9a11d1379a3dc31b32bfef27
ben <ben@Debian>
2022-09-16 18:18:23 +0200

    first commit

---
+++ src/test/Test.java
@@ -0,0 +1,15 @@
+package test;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Test extends Application {
+
+    public void start(Stage stage) {
+        stage.show();
+    }
+    public static void main(String[] args) {
+        launch();
+    }
+	
+}
