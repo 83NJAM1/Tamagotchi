1f2f65630c138fa47e5ff9277ae85466a956fc12
ambryal <ambryal@ambryal-293373>
2022-09-17 10:33:07 +0200

    lol

--- src/test/Test.java
+++ src/test/Test.java
@@ -83,7 +83,7 @@ public class Test extends Application {
     }
     
     public static void main(String[] args) {
-    	System.out.println("j'ai fixé le problème sans tout supprimer :)");
+    	System.out.println("j'ai fixé le problème sans tout supprimer :) lol");
         launch();
     }
 	
