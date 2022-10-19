9f9fafb5c7e163776bfed4f2f78456b5fa876142
KiKi-ki3 <urien.kian@orange.fr>
2022-10-19 15:48:30 +0200

    indentation game

--- src/app/model/Game.java
+++ src/app/model/Game.java
@@ -105,15 +105,15 @@ public class Game {
 		
 		for(int i=0;i<1000000;i++) {
 			refresh();
-			getTextInfo();
+		getTextInfo();
 		
-			String input = scanner.nextLine();
-			int n = 0;
-			if(input.length()==2) n = input.charAt(1)-'0';
+		String input = scanner.nextLine();
+		int n = 0;
+		if(input.length()==2) n = input.charAt(1)-'0';
 		
-			Action action=getEnabledActions().get(inputTable.indexOf(String.valueOf(input.charAt(0))));
-			refresh();
-			action.execute(n);
+		Action action=getEnabledActions().get(inputTable.indexOf(String.valueOf(input.charAt(0))));
+		refresh();
+		action.execute(n);
 		}
 		scanner.close();
 		
