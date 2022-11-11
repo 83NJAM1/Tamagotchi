7f06d17e8282eb84201849e8affe3a4db847cffa
benjamin <benjamin.leplenier@gmail.com>
2022-11-11 17:12:25 +0100

    update - correction d'erreur

---
+++ src/app/model/Robot.java
@@ -0,0 +1,15 @@
+package app.model;
+
+/**
+ * 
+ * @author ben
+ * L'animal à prendre soin
+ */
+public class Robot implements Pet {
+
+	// il y aura plusieurs Stat, restons avec une pour le moment
+	// TODO choisir une strucutre de donnée pour les stocker
+	//      ou on les liste une par une sans structure de donnée
+	// ATTENTION: Reference partagé avec constroller.Stat
+	private Stat test;
+}
